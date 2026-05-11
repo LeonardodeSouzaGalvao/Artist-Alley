d8c96f3a56239d1f3ffa31f0ff212dc5b934d71c
Leonardo de Souza Galvão <160603231+LeonardodeSouzaGalvao@users.noreply.github.com>
2026-05-11 10:06:35 -0300

    Feat: Create de commission slots

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
+import commissionSlotRoutes from './routes/commissionSlots';
 import { authMiddleware } from './middleware/auth';
 
 dotenv.config();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get('/', (_req, res) => res.json({ message: 'O servidor está rodando!' }));
 
 app.use('/auth', authRoutes);
+app.use('/commission-slots', commissionSlotRoutes);
 
 app.get('/me', authMiddleware, (req, res) => {
   const user = (req as any).user;
---
+++ backend/src/services/commissionSlotService.ts
@@ -0,0 +1,44 @@
+import {CommissionSlot as PrismaCommissionSlot } from '@prisma/client';
+import { prisma } from '../lib/prisma';
+
+export type CommissionSlot = { id: string; title: string; description: string; price: number ; imageUrl?: string; slots: number; available: boolean };
+
+function toCommissionSlot(commissionSlot: PrismaCommissionSlot): CommissionSlot {
+  return {
+    id: commissionSlot.id,
+    title: commissionSlot.title,
+    description: commissionSlot.description ?? '',
+    price: commissionSlot.price.toNumber(),
+    imageUrl: commissionSlot.imageUrl ?? undefined,
+    slots: commissionSlot.slots,
+    available: commissionSlot.available,
+  };
+}
+
+export async function createCommissionSlot(title: string, description: string, price: number, artistId: string, slots: number, imageUrl?: string) {
+  const slot = await prisma.commissionSlot.create({
+    data: {
+        title,
+        description,
+        price,
+        slots,
+        available: true,
+        imageUrl: imageUrl ?? '',
+        artist: {
+          connect: { id: artistId },
+        },
+    },
+  });
+
+  return toCommissionSlot(slot);
+}
+
+export async function findByArtistId(artistId: string) {
+  const slot = await prisma.commissionSlot.findFirst({ where: { artistId } });
+  return slot ? toCommissionSlot(slot) : null;
+}
+
+export async function findById(id: string) {
+  const slot = await prisma.commissionSlot.findUnique({ where: { id } });
+  return slot ? toCommissionSlot(slot) : null;
+}
